3a8acc4169bddb881f186af640ab8269ec82c1c2
googlebrain <ritu@reckonsys.com>
2019-06-09 17:40:08 +0530

    added new end points - user login and user registeration - TOken based

--- server/cast/api_urls.py
+++ server/cast/api_urls.py
@@ -1,8 +1,21 @@
+__author__ = "RituRaj"
+
 from django.conf.urls import url
+
 import code_analyze.views
+import cast_auth.views
 
 urlpatterns = [
     url('code_analyze',
         code_analyze.views.CodeAnalyzeApi.as_view(),
-        name="code_analyze")
+        name="code_analyze"),
+
+    # Login
+    url('user/login/',
+        cast_auth.views.UserAuthLoginView.as_view(), name="login"),
+
+    # Register a new user
+    url('user/register',
+        cast_auth.views.UserRegisterationView.as_view(), name="register_user"),
+
 ]

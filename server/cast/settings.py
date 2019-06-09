dc04350813862ac3e9884cb95863b820735ea29c
googlebrain <ritu@reckonsys.com>
2019-06-09 14:08:22 +0530

    register new app - code_analyze and rest_framework in installed apps

--- server/cast/settings.py
+++ server/cast/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local -apps
+    'code_analyze',
+    # 3RD PARTY
+    'rest_framework',
 ]
 
 MIDDLEWARE = [

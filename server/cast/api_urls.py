13e53a0eacfb4ddc3da0e7ff04be1e407e8750a6
googlebrain <ritu@reckonsys.com>
2019-06-09 16:18:13 +0530

    remane api end points

--- server/cast/api_urls.py
+++ server/cast/api_urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 import code_analyze.views
 
 urlpatterns = [
-    url('analyze',
+    url('code_analyze',
         code_analyze.views.CodeAnalyzeApi.as_view(),
         name="code_analyze")
 ]

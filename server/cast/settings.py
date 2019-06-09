37ce0e4d044170ad3d9165d91c95a3400caa5186
googlebrain <ritu@reckonsys.com>
2019-06-09 16:16:57 +0530

    added new module -cors headers for cross-allow_origin

--- server/cast/settings.py
+++ server/cast/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'code_analyze',
     # 3RD PARTY
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS HEADERS
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'cast.urls'
 

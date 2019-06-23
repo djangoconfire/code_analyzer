c3bf0da106f19c98d10234a0f0976d60f3777987
djangoconfire <djangopycon@gmail.com>
2019-06-23 21:08:37 +0530

    updated api end points

--- server/cast/urls.py
+++ server/cast/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('api/', include(('cast.api_urls', 'cast'), namespace="api"))
